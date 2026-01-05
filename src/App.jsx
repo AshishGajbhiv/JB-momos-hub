33362ee38b2edddf82e9b441ce6809d220020a20
AshishGajbhiv <ashish28gajbhiv@gmail.com>
2026-01-06 02:05:52 +0530

    fix(deployment): use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/JB-momos-hub">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
